71289f4345e8b949c3a3bedcd4cf4d684fe0fb19
Mahim Safa <mahimsafa@gmail.com>
2026-06-07 23:01:04 +0600

    feat: Update Celery configuration and dependencies for SSL support
    This commit enhances the Celery application by adding SSL support with certificate verification disabled. It also updates the dependencies in `pyproject.toml` to their latest versions, ensuring compatibility and improved performance. Additionally, a new `uv.lock` file is introduced to manage package resolutions.

--- reface-backend/celery_app.py
+++ reface-backend/celery_app.py
@@ -1,5 +1,7 @@
+import ssl
 from celery import Celery
 from core.config import settings
+
 
 celery_app = Celery(
     "reface",
@@ -11,6 +13,11 @@ celery_app = Celery(
     ],
 )
 
+# Explicitly set cert_reqs to NONE
+ssl_skip_verify = {
+    'ssl_cert_reqs': ssl.CERT_NONE
+}
+
 celery_app.conf.update(
     task_serializer="json",
     accept_content=["json"],
@@ -20,4 +27,6 @@ celery_app.conf.update(
     task_track_started=True,
     task_acks_late=True,
     worker_prefetch_multiplier=1,
+    broker_use_ssl=ssl_skip_verify,
+    redis_backend_use_ssl=ssl_skip_verify
 )
